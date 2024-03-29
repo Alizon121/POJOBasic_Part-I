aa0d86a4fa76af2fd0a582357d8fb85541500ae9
Cannicakee <stanfill11@gmail.com>
2024-03-28 20:55:19 -0600

    Completed Problem 3

--- problems/03-set-key-in-object.js
+++ problems/03-set-key-in-object.js
@@ -9,13 +9,17 @@ Examples:
 let obj = {}
 setKeyInObject(obj, "apple", "yum"); // => {apple: "yum"}
 
-let obj1 = {str: "hello"}
+
 setKeyInObject(obj1, "num", 3); // => {str: "hello", num: 3}
 ***********************************************************************/
+let obj = {}
+let obj1 = {str: "hello"}
 
 function setKeyInObject(obj, string, value) {
-  // Your code here 
+  obj[string] = value;
+  return obj;
 }
-
+console.log(setKeyInObject(obj, "apple", "yum")); // => {apple: "yum"\
+console.log(setKeyInObject(obj1, "num", 3)); // => {str: "hello", num: 3}
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = setKeyInObject;
