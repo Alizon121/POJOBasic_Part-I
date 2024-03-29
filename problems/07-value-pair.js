08ceb4895318b301e5744a1775aecdd43bd6ef57
Andrew <andrew.lizon12@gmail.com>
2024-03-28 21:05:30 -0700

    Completed Problem 7

--- problems/07-value-pair.js
+++ problems/07-value-pair.js
@@ -10,9 +10,24 @@ valuePair(object1, object2, 'location'); // => [ 'NY', 'SF' ]
 valuePair(object1, object2, 'name'); // => [ 'One', 'Two' ]
 ***********************************************************************/
 
+let object1 = {name: 'One', location: 'NY', age: 3};
+let object2 = {name: 'Two', location: 'SF'};
+
+
 function valuePair(obj1, obj2, key) {
-  // Your code here 
+
+  let newArr = [];
+for (let keys in obj1 && obj2) {
+
+  if (key.includes(keys)) {
+    newArr.push(obj1[keys], obj2[keys])
+  }
+}
+return newArr
 }
 
+
+console.log(valuePair(object1, object2, 'location')); // => [ 'NY', 'SF' ]
+console.log(valuePair(object1, object2, 'name')); // => [ 'One', 'Two' ]
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = valuePair;
