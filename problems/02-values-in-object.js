d49d27b6536e5b83a3f9cf84452375461dfbd551
Andrew <andrew.lizon12@gmail.com>
2024-03-28 19:35:21 -0700

    Incomplete Problem 2

--- problems/02-values-in-object.js
+++ problems/02-values-in-object.js
@@ -14,9 +14,28 @@ valuesInObject(animals); // => ["Wolfie", "Jet", "Bilbo"]
 valuesInObject(foods); // => ["tart", "sour", "sweet"]
 ***********************************************************************/
 
+let animals = {dog: "Wolfie", cat: "Jet", bison: "Bilbo"}
+let foods = {apple: "tart", lemon: "sour", mango: "sweet"}
+
+// function valuesInObject(obj) {
+
+// let values = Object.values(obj);
+// console.log(values);
+
+// }
+
 function valuesInObject(obj) {
-  // Your code here 
+
+let newArr = [];
+for (let value in obj) {
+
+newArr.push(value);
+}
+return newArr;
 }
 
+
+valuesInObject(animals); // => ["Wolfie", "Jet", "Bilbo"]
+valuesInObject(foods); // => ["tart", "sour", "sweet"]
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = valuesInObject;
