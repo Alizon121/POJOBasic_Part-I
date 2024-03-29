b50f71b20c4f4e787089e4e54d1a668f1bc32134
Cannicakee <stanfill11@gmail.com>
2024-03-28 21:35:05 -0600

    Completed Problem 5

--- problems/05-does-key-exist.js
+++ problems/05-does-key-exist.js
@@ -9,10 +9,19 @@ let obj1 = {bootcamp: 'App Academy', course: 'Bootcamp Prep'}
 doesKeyExist(obj1, 'course'); // => true
 doesKeyExist(obj1, 'name'); // => false
 ***********************************************************************/
+let obj1 = {bootcamp: 'App Academy', course: 'Bootcamp Prep'}
 
 function doesKeyExist(obj, key) {
-  // Your code here 
+  for(let keys in obj) {
+    if(keys.includes(key)) {
+      return true;
+
+    }
+  }
+  return false;
 }
 
+console.log(doesKeyExist(obj1, 'course')); // => true
+console.log(doesKeyExist(obj1, 'name')); // => false
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = doesKeyExist;
