24e227ec210fd2e8fc44659665ed3334655a0f0b
Andrew <andrew.lizon12@gmail.com>
2024-03-28 19:08:13 -0700

    Add .gitignore

--- problems/01-keys-in-object.js
+++ problems/01-keys-in-object.js
@@ -13,7 +13,11 @@ keysInObject(foods); // => ["apple", "lemon", "mango"]
 ***********************************************************************/
 
 function keysInObject(obj) {
-  // Your code here 
+
+
+
+
+  
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
